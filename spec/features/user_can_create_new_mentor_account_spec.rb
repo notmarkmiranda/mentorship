fb7141d24661dad0cd1ffd723d9dcce1d8bc5df2
Josh Washke <jwashke@icloud.com>
2016-08-01 15:08:39 -0600

    Create feature test for user creates a new mentor

--- spec/features/user_can_create_new_mentor_account_spec.rb
+++ spec/features/user_can_create_new_mentor_account_spec.rb
@@ -2,6 +2,19 @@ require "rails_helper"
 
 RSpec.feature "User can create a new mentor account" do
   scenario "The information is updated and they are taken back to their dashboard" do
-    
+    user = create(:user)
+    ApplicationController.any_instance.stub(:current_user).and_return(user)
+
+    visit new_mentor_path
+
+    fill_in "Location", with: "Denver"
+    select "MT", from: "Timezone"
+
+    click_button "Create Mentor Profile"
+
+    click_link "Browse Mentors"
+
+    expect(page).to have_content(user.name)
+    expect(page).to have_content("Denver")
   end
 end
