ef6f2442ab989ac8d080d5d69a3d51b2683b0183
Josh Washke <jwashke@icloud.com>
2016-08-01 10:57:01 -0600

    Remove view spec

--- spec/models/mentor_spec.rb
+++ spec/models/mentor_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe Mentor, type: :model do
     it { is_expected.to validate_presence_of :mentor_timezone }
     it { is_expected.to validate_presence_of :user }
   end
+
+  describe "#name" do
+    it "delegates the call to the user association" do
+      mentor = create(:mentor)
+
+      expect(mentor.name).to eq(mentor.user.name)
+    end
+  end
+
+  describe "#timezone" do
+    it "delegates the call to the mentor timezone association" do
+      mentor = create(:mentor)
+
+      expect(mentor.timezone).to eq(mentor.mentor_timezone.name)
+    end
+  end
+
+  describe "#picture" do
+    it "delegates the call to the user association" do
+      mentor = create(:mentor)
+
+      expect(mentor.picture).to eq(mentor.user.picture)
+    end
+  end
 end
